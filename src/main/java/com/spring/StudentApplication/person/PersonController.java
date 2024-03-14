f1dafc7f5746f5437d4367b96c4d3eb43a7a8963
Urte <urte.nistirenko@gmail.com>
2024-03-14 16:32:41 +0200

    add personController

---
+++ src/main/java/com/spring/StudentApplication/person/PersonController.java
@@ -0,0 +1,29 @@
+package com.spring.StudentApplication.person;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("api/persons")
+public class PersonController {
+    private PersonService service;
+
+    @Autowired
+    public PersonController(PersonService service) {
+        this.service = service;
+    }
+
+    @GetMapping(produces = "applicaton/json")
+    ResponseEntity<List<Person>> getAllPersons() {
+        List<Person> list = service.getAll();
+        return new ResponseEntity<>(list, HttpStatus.OK);}
+
+}
